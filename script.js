9242463ff0a25c051c26726a70738665fdd29396
austinatkinson93 <austinatkinson93@users.noreply.github.com>
2020-01-23 18:15:04 -0500

    working table and list of frameworks

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 
 // variables
 var titles = []
-var languages = ["PHP", "Java", "HTML, CSS", "JavaScript", "C++", "ASP.net"];
+var tableInfoArray = []
+var languages = ["PHP", "Java", "HTML, CSS", "JavaScript", "C++", "Python"];
 
-// function containing ajax call
+// functions containing ajax calls //
+
+// function that creates our list of frameworks
 function getFrameworkInfo(wikipediaTableHeaderName, cb) {
     //everything inside of this function works, we just need to figure out the return of the array
     var frameworkInfo = [];
@@ -18,6 +21,7 @@ function getFrameworkInfo(wikipediaTableHeaderName, cb) {
         var id = "#" + wikipediaTableHeaderName.replace(" ", "_");
         var javaHeader = html.find(id);
         var javaTableRows = javaHeader.parent().next().next().children("tbody").children("tr");
+        console.log(html)
 
         javaTableRows.each(function (i) {
             //skip the first header that says "Project"
@@ -38,9 +42,52 @@ function getFrameworkInfo(wikipediaTableHeaderName, cb) {
 
 }
 
+// function that creates our framework tables
+function getFrameworkTable(frameworkLink, cb) {
+    //everything inside of this function works
+    var queryURL = "https://cors-anywhere.herokuapp.com/" + frameworkLink
+    tableInfoArray = []
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (res) {
+        //create a jquery object with the response that we get 
+        var html = $(res);
+        //use the param to create an class tag
+        var tableClass = ".infobox"
+        var infoTable = html.find(tableClass);
+        var infoTableHeader = html.find(".summary").text()
+        var infoTableRows = infoTable.children("tbody").children("tr")
+        console.log(infoTableRows)
+
+        infoTableRows.each(function (i) {
+
+            //get the text content of each row that we found
+            var currentRowTitle = $(this).find("th").text();
+            var currentRowInfo = $(this).find("td").text()
 
 
+            if (currentRowTitle != "" && currentRowInfo != "") {
+                tableInfoArray.push({
+                    title: currentRowTitle,
+                    info: currentRowInfo
+                });
 
+            }
+
+        });
+
+        cb(tableInfoArray);
+
+    });
+
+}
+
+
+// Functions without ajax calls
+
+// function that dynamically creates our buttons on load
 function createButtons(array) {
     var body = $("#mainBody")
 
@@ -58,32 +105,55 @@ function createButtons(array) {
 createButtons(languages)
 
 
+// event listeners //
 
-// event listeners
+// Creates the framworks table based on language selected
 $(".button").click(function (event) {
     event.preventDefault();
     titles = [];
     $("#table").empty()
+
     var id = $(event.target).attr("id");
 
     getFrameworkInfo(id, function (results) {
-       
+
         results.forEach(function (result) {
             titles.push(result);
         });
 
-        console.log(titles)
+        titles.forEach(function (item, i) {
+            var frameworkTitle = $("<tr>").text(titles[i].name).attr("href", titles[i].link);
+            $(frameworkTitle).attr("id", titles[i].name).attr("class", "framework")
 
-        titles.forEach( function (item, i) {
-            var frameworkTitle = $("<a>").text(titles[i].name).attr("href", titles[i].link);
-            $(frameworkTitle).attr("id", titles[i].name);
+            $("#table").append(frameworkTitle);
 
-            $("#table").append(frameworkTitle)
-           
-            console.log(titles[i].name);
         })
     });
 
 
 })
 
+// creates a table of basic information based on framework selected.
+$("#table").on("click", ".framework", function () {
+    $("#table").empty()
+
+    var frameworkLink = $(this).attr("href")
+    getFrameworkTable(frameworkLink, function () {
+
+
+        console.log(tableInfoArray)
+
+        tableInfoArray.forEach(function (item, i) {
+            var row =$("<tr>").attr("id", "row"+[i])
+            var tableTitle = $("<th>").text(tableInfoArray[i].title);
+            var tableInfo = $("<td>").text(tableInfoArray[i].info);
+
+            $("#table").append(row);
+            $("#row"+[i]).append(tableTitle);
+            $("#row"+[i]).append(tableInfo);
+
+
+            console.log("hi")
+        })
+    })
+})
